b6a0fe2bf7789c7a008a211ebc683c84a3a0034a
Renukadeveloper96 <renukamaradagi@gmail.com>
2022-09-16 22:09:59 +0530

    jdbc

---
+++ src/main/java/com/learning/controller/RestControllers.java
@@ -0,0 +1,35 @@
+package com.learning.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestControllers {
+	
+	@GetMapping(value="/greet/{input}")
+	public String greetMessage(@PathVariable String input) {
+		return "Hello " + input;
+	}
+	
+	@GetMapping(value="/greet")
+	public String greetMessage1(@RequestParam String input) {
+		return "Hello " + input;
+	}
+	@GetMapping("/printTable/{number}")
+	public String PrintTable(@PathVariable int number) {
+		String result="";
+		
+		for(int i=1;i<=10;i++) {
+			result+=number+" X "+ i+" = "+number*i+"\n";
+		
+		}
+		return result;
+	}
+
+//	@PostMapping(value="/greeting")
+//	public String greetMessage1(@RequestParam String input) {
+//		return "Hello " + input;
+//	}
+}
