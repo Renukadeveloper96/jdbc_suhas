696cc6e67e9b609edccc917a3c34b8ded514d108
Renukadeveloper96 <renukamaradagi@gmail.com>
2022-09-24 13:18:11 +0530

    vscode

--- src/main/java/com/learning/controller/RestControllers.java
+++ src/main/java/com/learning/controller/RestControllers.java
@@ -17,7 +17,7 @@ public class RestControllers {
 	public String greetMessage1(@RequestParam String input) {
 		return "Hello " + input;
 	}
-	@GetMapping("/printTable/{number}")
+	@GetMapping("/printTables/{number}")
 	public String PrintTable(@PathVariable int number) {
 		String result="";
 		
